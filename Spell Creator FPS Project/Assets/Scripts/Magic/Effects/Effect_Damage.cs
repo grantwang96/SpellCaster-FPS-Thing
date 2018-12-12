12da0a513180853035bf0282ee72844da0a3aa59
grantwang96 <gw805@nyu.edu>
2018-12-12 08:18:30 -0800

    Created design doc and added Elements

--- Spell Creator FPS Project/Assets/Scripts/Magic/Effects/Effect_Damage.cs
+++ Spell Creator FPS Project/Assets/Scripts/Magic/Effects/Effect_Damage.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Basic damage effect
+/// </summary>
 public class Effect_Damage : Spell_Effect {
 
     [SerializeField] private int _basePower;
     public int BasePower { get { return _basePower; } }
+    [SerializeField] private Element _element;
+    public Element Element { get { return _element; } }
 
     public override void TriggerEffect(IDamageable damageable, ISpellCaster caster) {
         Debug.Log("Applied damage: " + BasePower);
---
+++ Spell Creator FPS Project/Assets/Scripts/Magic/Element.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Element {
+    Neutral, Fire, Ice, Thunder
+}
